92030a68108f52d864612fa33ab2748759830cc7
TravisWongX <travis.x.wong@gmail.com>
2022-02-08 17:46:32 +0800

    计数器

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,13 @@
 Page({
   data: {
     message: 'hello world!',
-    movies: ['蜘蛛侠', '钢铁侠', '咸蛋超人']
+    movies: ['蜘蛛侠', '钢铁侠', '咸蛋超人'],
+    counter: 0
   },
+  handleIncrement: function() {
+    this.setData({ counter: this.data.counter + 1})
+  },
+  handleDecrement: function() {
+    this.setData({ counter: this.data.counter - 1})
+  }
 })
